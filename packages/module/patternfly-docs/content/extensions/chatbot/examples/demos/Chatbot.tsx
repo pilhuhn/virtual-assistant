4dc1bb9fb82e3f11d9911c695866c8b131b796b9
Heiko W. Rupp <hwr@pilhuhn.de>
2025-02-17 16:41:11 +0100

    Hook up via WebService

--- packages/module/patternfly-docs/content/extensions/chatbot/examples/demos/Chatbot.tsx
+++ packages/module/patternfly-docs/content/extensions/chatbot/examples/demos/Chatbot.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */ // TODO remove
+
 import React from 'react';
 
 import { Bullseye, Brand, DropdownList, DropdownItem, DropdownGroup, SkipToContent } from '@patternfly/react-core';
@@ -36,10 +38,10 @@ import userAvatar from '../Messages/user_avatar.svg';
 import patternflyAvatar from '../Messages/patternfly_avatar.jpg';
 
 const footnoteProps = {
-  label: 'Lightspeed uses AI. Check for mistakes.',
+  label: 'ChatBot uses AI. Check for mistakes.',
   popover: {
     title: 'Verify accuracy',
-    description: `While Lightspeed strives for accuracy, there's always a possibility of errors. It's a good practice to verify critical information from reliable sources, especially if it's crucial for decision-making or actions.`,
+    description: `While ChatBot strives for accuracy, there's always a possibility of errors. It's a good practice to verify critical information from reliable sources, especially if it's crucial for decision-making or actions.`,
     bannerImage: {
       src: 'https://cdn.dribbble.com/userupload/10651749/file/original-8a07b8e39d9e8bf002358c66fce1223e.gif',
       alt: 'Example image for footnote popover'
@@ -57,42 +59,6 @@ const footnoteProps = {
   }
 };
 
-const markdown = `A paragraph with *emphasis* and **strong importance**.
-
-> A block quote with ~strikethrough~ and a URL: https://reactjs.org.
-
-Here is an inline code - \`() => void\`
-
-Here is some YAML code:
-
-~~~yaml
-apiVersion: helm.openshift.io/v1beta1/
-kind: HelmChartRepository
-metadata:
-  name: azure-sample-repo0oooo00ooo
-spec:
-  connectionConfig:
-  url: https://raw.githubusercontent.com/Azure-Samples/helm-charts/master/docs
-~~~
-
-Here is some JavaScript code:
-
-~~~js
-import React from 'react';
-
-const MessageLoading = () => (
-  <div className="pf-chatbot__message-loading">
-    <span className="pf-chatbot__message-loading-dots">
-      <span className="pf-v6-screen-reader">Loading message</span>
-    </span>
-  </div>
-);
-
-export default MessageLoading;
-
-~~~
-`;
-
 // It's important to set a date and timestamp prop since the Message components re-render.
 // The timestamps re-render with them.
 const date = new Date();
@@ -101,40 +67,13 @@ const initialMessages: MessageProps[] = [];
 
 const welcomePrompts = [
   {
-    title: 'Topic 1',
-    message: 'Helpful prompt for Topic 1'
-  },
-  {
-    title: 'Topic 2',
-    message: 'Helpful prompt for Topic 2'
+    title: 'PFBot',
+    message: 'I am your helpful chatbot, ready to tell you about PatternFly'
   }
+
 ];
 
-const initialConversations = {
-  Today: [{ id: '1', text: 'Hello, can you give me an example of what you can do?' }],
-  'This month': [
-    {
-      id: '2',
-      text: 'Enterprise Linux installation and setup'
-    },
-    { id: '3', text: 'Troubleshoot system crash' }
-  ],
-  March: [
-    { id: '4', text: 'Ansible security and updates' },
-    { id: '5', text: 'Red Hat certification' },
-    { id: '6', text: 'Lightspeed user documentation' }
-  ],
-  February: [
-    { id: '7', text: 'Crashing pod assistance' },
-    { id: '8', text: 'OpenShift AI pipelines' },
-    { id: '9', text: 'Updating subscription plan' },
-    { id: '10', text: 'Red Hat licensing options' }
-  ],
-  January: [
-    { id: '11', text: 'RHEL system performance' },
-    { id: '12', text: 'Manage user accounts' }
-  ]
-};
+const initialConversations = {};
 
 export const ChatbotDemo: React.FunctionComponent = () => {
   const [chatbotVisible, setChatbotVisible] = React.useState<boolean>(true);
@@ -153,35 +92,79 @@ export const ChatbotDemo: React.FunctionComponent = () => {
   const historyRef = React.useRef<HTMLButtonElement>(null);
 
   const WS_URL = 'ws://127.0.0.1:8080/websocket';
-  const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(WS_URL, {
-    share: false,
-    shouldReconnect: () => true
-  });
+  const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(
+    WS_URL,
+    {
+      share: false,
+      shouldReconnect: () => false
+    },
+    chatbotVisible // only connect if the bot is visible
+  );
 
   // Auto-scrolls to the latest message
   React.useEffect(() => {
     // don't scroll the first load - in this demo, we know we start with two messages
+    console.log('messages number is ' + messages.length);
     if (messages.length > 2) {
       scrollToBottomRef.current?.scrollIntoView({ behavior: 'smooth' });
     }
   }, [messages]);
 
   React.useEffect(() => {
-    console.log('Connection state changed');
     if (readyState === ReadyState.OPEN) {
-      sendJsonMessage({
-        event: 'subscribe',
-        data: {
-          channel: 'general-chatroom'
-        }
-      });
+      console.log('WS Ready');
+      // We can use this to start a general inquiry or such
+      // sendJsonMessage({
+      //   event: 'subscribe',
+      //   data: {
+      //     channel: 'general-chatroom'
+      //   }
+      // });
+    } else {
+      console.log('WS Connection state changed to ' + JSON.stringify(readyState));
     }
   }, [readyState]);
 
   // Run when a new WebSocket message is received (lastJsonMessage)
   React.useEffect(() => {
-    console.log(`Got a new message: ${lastJsonMessage.choices}`);
-
+    if (lastJsonMessage == null) {
+      console.log('LastJsonMessage is null');
+      return;
+    }
+    console.log(`Got a new message: + ` + JSON.stringify(lastJsonMessage));
+    // TODO welcome message from bot?
+    const newMessages: MessageProps[] = [];
+    messages.forEach((msg: MessageProps) => newMessages.push(msg));
+    const loadedMessages: MessageProps[] = [];
+    // We can't use structuredClone since messages contains functions, but we can't mutate
+    // items that are going into state or the UI won't update correctly
+    newMessages.forEach((msg) => loadedMessages.push(msg));
+    loadedMessages.pop();
+    loadedMessages.push({
+      id: generateId(),
+      role: 'bot',
+      content: lastJsonMessage.data,
+      name: 'Bot',
+      isLoading: false,
+      avatar: patternflyAvatar,
+      timestamp: date.toLocaleString(),
+      actions: {
+        // eslint-disable-next-line no-console
+        positive: { onClick: () => console.log('Good response') },
+        // eslint-disable-next-line no-console
+        negative: { onClick: () => console.log('Bad response') },
+        // eslint-disable-next-line no-console
+        copy: { onClick: () => console.log('Copy') },
+        // eslint-disable-next-line no-console
+        share: { onClick: () => console.log('Share') },
+        // eslint-disable-next-line no-console
+        listen: { onClick: () => console.log('Listen') }
+      }
+    });
+    setMessages(loadedMessages);
+    // make announcement to assistive devices that new message has loaded
+    setAnnouncement(`Message from Bot: API response goes here`);
+    setIsSendButtonDisabled(false);
   }, [lastJsonMessage]);
 
   const onSelectModel = (
@@ -209,7 +192,7 @@ export const ChatbotDemo: React.FunctionComponent = () => {
     const newMessages: MessageProps[] = [];
     // We can't use structuredClone since messages contains functions, but we can't mutate
     // items that are going into state or the UI won't update correctly
-    messages.forEach((message) => newMessages.push(message));
+    messages.forEach((msg: MessageProps) => newMessages.push(msg));
     // It's important to set a timestamp prop since the Message components re-render.
     // The timestamps re-render with them.
     const date = new Date();
@@ -235,40 +218,6 @@ export const ChatbotDemo: React.FunctionComponent = () => {
     setMessages(newMessages);
     // make announcement to assistive devices that new messages have been added
     setAnnouncement(`Message from User: ${message}. Message from Bot is loading.`);
-
-    // this is for demo purposes only; in a real situation, there would be an API response we would wait for
-    setTimeout(() => {
-      const loadedMessages: MessageProps[] = [];
-      // We can't use structuredClone since messages contains functions, but we can't mutate
-      // items that are going into state or the UI won't update correctly
-      newMessages.forEach((message) => loadedMessages.push(message));
-      loadedMessages.pop();
-      loadedMessages.push({
-        id: generateId(),
-        role: 'bot',
-        content: 'API response goes here',
-        name: 'Bot',
-        isLoading: false,
-        avatar: patternflyAvatar,
-        timestamp: date.toLocaleString(),
-        actions: {
-          // eslint-disable-next-line no-console
-          positive: { onClick: () => console.log('Good response') },
-          // eslint-disable-next-line no-console
-          negative: { onClick: () => console.log('Bad response') },
-          // eslint-disable-next-line no-console
-          copy: { onClick: () => console.log('Copy') },
-          // eslint-disable-next-line no-console
-          share: { onClick: () => console.log('Share') },
-          // eslint-disable-next-line no-console
-          listen: { onClick: () => console.log('Listen') }
-        }
-      });
-      setMessages(loadedMessages);
-      // make announcement to assistive devices that new message has loaded
-      setAnnouncement(`Message from Bot: API response goes here`);
-      setIsSendButtonDisabled(false);
-    }, 5000);
   };
 
   const findMatchingItems = (targetValue: string) => {
@@ -336,7 +285,9 @@ export const ChatbotDemo: React.FunctionComponent = () => {
       <ChatbotToggle
         tooltipLabel="Chatbot"
         isChatbotVisible={chatbotVisible}
-        onToggleChatbot={() => setChatbotVisible(!chatbotVisible)}
+        onToggleChatbot={() => {
+          setChatbotVisible(!chatbotVisible);
+        }}
         id="chatbot-toggle"
         ref={toggleRef}
       />
